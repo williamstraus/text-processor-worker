118a710c100d6c10de5ba590d535747f69275e22
William Straus <wllam08@gmail.com>
2022-09-28 18:35:00 +0300

    added DB initial migration

--- src/main/java/com/wordprocessor/worker/DBConfig.java
+++ src/main/java/com/wordprocessor/worker/DBConfig.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 import javax.sql.DataSource;
 
@@ -20,6 +23,11 @@ public class DBConfig {
         ds.setUrl(env.getProperty("hsql.url"));
         ds.setUsername(env.getProperty("hsql.username"));
         ds.setPassword(env.getProperty("hsql.password"));
+
+        var populator = new ResourceDatabasePopulator(
+                new ClassPathResource("db/migration/V1_Create_table.sql"));
+
+        DatabasePopulatorUtils.execute(populator, ds);
 
         return ds;
     }
